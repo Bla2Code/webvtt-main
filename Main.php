423c08a5917779dcabd9b1b9388d0523665a90df
malexey <everdream.ml@gmail.com>
2019-05-28 17:47:06 +0300

    2 commet

--- Main.php
+++ Main.php
@@ -1,8 +1,10 @@
 <?php
 // composer require podlove/webvtt-parser
-require_once('vendor/podlove/webvtt-parser/src/Podlove/Webvtt/parser.php');
+require_once "vendor/podlove/webvtt-parser/src/Podlove/Webvtt/parser.php";
 
 $parser = new Podlove\Webvtt\Parser();
-$content = "WEBVTT\n\n00:00:00.000 --> 01:22:33.440\nHello world\n\n01:22:33.440 --> 01:22:34.440\n<v Eric>Hi again\n";
+//$content = "WEBVTT\n\n00:00:00.000 --> 01:22:33.440\nHello world\n\n01:22:33.440 --> 01:22:34.440\n<v Eric>Hi again\n";
+$content = file_get_contents("file/web.vtt");
+//print_r($content);
 $result = $parser->parse($content);
 print_r($result);
