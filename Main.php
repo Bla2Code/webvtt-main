9d1b2e648063324579ab23565cdee0ace5bdd874
malexey <everdream.ml@gmail.com>
2019-05-28 18:26:56 +0300

    3 commit

--- Main.php
+++ Main.php
@@ -1,9 +1,13 @@
 <?php
+/** После загрузки файла:
+ * - распарсить файл
+ * - перезаписать распарсенные данные в бд или сохранить новые
+ * - вернуть ответ об успешном выполнении или ошибку
+ */
 // composer require podlove/webvtt-parser
 require_once "vendor/podlove/webvtt-parser/src/Podlove/Webvtt/parser.php";
 
 $parser = new Podlove\Webvtt\Parser();
-//$content = "WEBVTT\n\n00:00:00.000 --> 01:22:33.440\nHello world\n\n01:22:33.440 --> 01:22:34.440\n<v Eric>Hi again\n";
 $content = file_get_contents("file/web.vtt");
 $result = $parser->parse($content);
 print_r($result);
